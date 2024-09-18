bdab755c6e3cece175a43cedf3a4d52bcae410c2
SzymonKraj <szymdziek@gmail.com>
2024-09-18 11:28:40 +0200

    brak modyfikatora dostepu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Osoba osobaJas = new Osoba(imie"Jas",nazwisko"Sliweczka");
+        System.out.println(osobaJas.imie);
+        System.out.println(osobaJas.nazwisko);
+        osobaJas.imie = "Janek";
+        System.out.println(osobaJas.imie);
     }
 }
--- src/Osoba.java
+++ src/Osoba.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Osoba {
+public class Osoba {
+    String imie;
+    String nazwisko;
+    int wiek;
+
+    //konstruktor to metoda wyowływana w momencie tworzenia obiektu
+
+    public Osoba(String imie, String nazwisko, int wiek) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
 }
